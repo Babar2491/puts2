7b83f3ae3cb763c350e07c8e581b3baf5acd0aee
Babar Khan <babarkhan2491@gmail.com>
2020-01-16 19:19:10 +0000

    return result up to 12 significant digits

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from __future__ import division
+
 from fractions import Fraction
 from flask import Flask, request
+
 
 app = Flask(__name__)
 
@@ -11,34 +14,70 @@ def index():
 
 @app.route('/add')
 def addition():
-    value1 = request.args.get('A', type=float) or request.args.get('A', type=Fraction) or 0.000
-    value2 = request.args.get('B', type=float) or request.args.get('B', type=Fraction) or 0.000
-    result = value1+value2
-    return '%0.3f \n' % result
+    value1 = request.args.get('A', type=int) or request.args.get('A', type=float) or request.args.get('A', type=Fraction) or 0.000
+    value2 = request.args.get('B', type=int) or request.args.get('B', type=float) or request.args.get('B', type=Fraction) or 0.000
+
+    has_float = isinstance(value1, (float, Fraction)) or isinstance(value2, (float, Fraction))
+
+    result = value1 + value2
+
+    if isinstance(result, Fraction):
+        result = float(result)
+
+    if has_float:
+        return str(float(format(result, ".12g")))
+    return str(format(result, '.12g'))
 
 
 @app.route('/subtract')
 def subtraction():
-    value1 = request.args.get('A', type=float) or request.args.get('A', type=Fraction) or 0.000
-    value2 = request.args.get('B', type=float) or request.args.get('B', type=Fraction) or 0.000
-    result = value1-value2
-    return '%0.3f \n' % result
+    value1 = request.args.get('A', type=int) or request.args.get('A', type=float) or request.args.get('A', type=Fraction) or 0.000
+    value2 = request.args.get('B', type=int) or request.args.get('B', type=float) or request.args.get('B', type=Fraction) or 0.000
+
+    has_float = isinstance(value1, (float, Fraction)) or isinstance(value2, (float, Fraction))
+
+    result = value1 - value2
+
+    if isinstance(result, Fraction):
+        result = float(result)
+
+    if has_float:
+        return str(float(format(result, ".12g")))
+    return str(format(result, '.12g'))
 
 
 @app.route('/multiply')
 def multiplication():
-    value1 = request.args.get('A', type=float) or request.args.get('A', type=Fraction) or 0.000
-    value2 = request.args.get('B', type=float) or request.args.get('B', type=Fraction) or 0.000
-    result = value1*value2
-    return '%0.3f \n' % result
+    value1 = request.args.get('A', type=int) or request.args.get('A', type=float) or request.args.get('A', type=Fraction) or 0.000
+    value2 = request.args.get('B', type=int) or request.args.get('B', type=float) or request.args.get('B', type=Fraction) or 0.000
+
+    has_float = isinstance(value1, (float, Fraction)) or isinstance(value2, (float, Fraction))
+
+    result = value1 * value2
+
+    if isinstance(result, Fraction):
+        result = float(result)
+
+    if has_float:
+        return str(float(format(result, ".12g")))
+    return str(format(result, '.12g'))
 
 
 @app.route('/divide')
 def division():
-    value1 = request.args.get('A', type=float) or request.args.get('A', type=Fraction) or 0.000
-    value2 = request.args.get('B', type=float) or request.args.get('B', type=Fraction) or 0.000
-    result = value1/value2
-    return '%0.3f \n' % result
+    value1 = request.args.get('A', type=int) or request.args.get('A', type=float) or request.args.get('A', type=Fraction) or 0.000
+    value2 = request.args.get('B', type=int) or request.args.get('B', type=float) or request.args.get('B', type=Fraction) or 0.000
+
+    has_float = isinstance(value1, (float, Fraction)) or isinstance(value2, (float, Fraction))
+
+    result = value1 / value2
+
+    if isinstance(result, Fraction):
+        result = float(result)
+
+    if has_float:
+        return str(float(format(result, ".12g")))
+    return str(format(result, '.12g'))
 
 if __name__ == "__main__":
     app.run()
